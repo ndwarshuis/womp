033ab093a5c62bde1d489c8a346502ebf2cbd654
ndwarshuis <ndwar@yavin4.ch>
2023-12-12 20:02:26 -0500

    ADD some nice exception handling

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,6 @@
 module Main (main) where
 
 import Control.Monad.Error.Class
-import Control.Monad.Trans.Except
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.Csv as C
@@ -105,6 +104,7 @@ dump =
       )
     <*> switch (long "force" <> short 'f' <> help "force retrieve")
 
+-- TODO not dry
 export :: Parser ExportOptions
 export =
   ExportOptions
@@ -114,6 +114,7 @@ export =
           <> metavar "CONFIG"
           <> help "path to config with schedules and meals"
       )
+    <*> dateInterval
 
 summarize :: Parser SummarizeOptions
 summarize =
@@ -124,22 +125,13 @@ summarize =
           <> metavar "CONFIG"
           <> help "path to config with schedules and meals"
       )
-    <*> ( (Just . readDay)
-            <$> strOption
-              ( long "start"
-                  <> short 's'
-                  <> metavar "START"
-                  <> help "start date on which to begin summary calculations"
-              )
-        )
-    <*> ( (Just . readDay)
-            <$> strOption
-              ( long "end"
-                  <> short 'e'
-                  <> metavar "END"
-                  <> help "end date on which to stop summary calculations (exclusive)"
-              )
-        )
+    <*> dateInterval
+
+dateInterval :: Parser DateIntervalOptions
+dateInterval =
+  DateIntervalOptions
+    <$> startDay
+    <*> endDay
     <*> option
       auto
       ( long "days"
@@ -149,6 +141,30 @@ summarize =
           <> value 7
       )
 
+startDay :: Parser (Maybe Day)
+startDay =
+  fmap readDay
+    <$> ( optional $
+            strOption
+              ( long "start"
+                  <> short 's'
+                  <> metavar "START"
+                  <> help "start date on which to begin summary calculations"
+              )
+        )
+
+endDay :: Parser (Maybe Day)
+endDay =
+  fmap readDay
+    <$> ( optional $
+            strOption
+              ( long "end"
+                  <> short 'e'
+                  <> metavar "END"
+                  <> help "end date on which to stop summary calculations (exclusive)"
+              )
+        )
+
 parse :: MonadUnliftIO m => Options -> m ()
 parse (Options c s) = do
   logOpts <-
@@ -156,11 +172,15 @@ parse (Options c s) = do
       <$> logOptionsHandle stderr False
   withLogFunc logOpts $ \lf -> do
     env <- mkSimpleApp lf Nothing
-    runRIO env $ case s of
+    runRIO env $ handle err $ case s of
       Fetch o -> runFetch c o
       Dump o -> runDump c o
       Export o -> runExport c o
       Summarize o -> runSummarize c o
+  where
+    err (AppException es) = do
+      mapM_ (logError . displayBytesUtf8 . encodeUtf8) $ concatMap showError es
+      exitFailure
 
 data Options = Options CommonOptions SubCommand
 
@@ -184,13 +204,20 @@ data FetchOptions = FetchOptions {foID :: !FID, foForce :: !Bool}
 
 data DumpOptions = DumpOptions {doID :: !FID, doForce :: !Bool}
 
-data ExportOptions = ExportOptions {eoConfig :: !FilePath}
+data ExportOptions = ExportOptions
+  { eoConfig :: !FilePath
+  , eoDateInterval :: !DateIntervalOptions
+  }
 
 data SummarizeOptions = SummarizeOptions
   { soConfig :: !FilePath
-  , soStart :: Maybe Day
-  , soEnd :: Maybe Day
-  , soDays :: Int
+  , soDateInterval :: !DateIntervalOptions
+  }
+
+data DateIntervalOptions = DateIntervalOptions
+  { dioStart :: Maybe Day
+  , dioEnd :: Maybe Day
+  , dioDays :: Int
   }
 
 readDay :: String -> Day
@@ -233,25 +260,30 @@ runDump CommonOptions {coKey} DumpOptions {doID, doForce} = do
       liftIO $ TI.putStr j
 
 runExport :: CommonOptions -> ExportOptions -> RIO SimpleApp ()
-runExport co ExportOptions {eoConfig} = do
-  rs <- readRowNutrients co eoConfig _
+runExport co ExportOptions {eoConfig, eoDateInterval} = do
+  dayspan <- dateIntervalToDaySpan eoDateInterval
+  rs <- readRowNutrients co eoConfig dayspan
   BL.putStr $ C.encodeWith tsvOptions rs
-  return ()
 
-readRowNutrients :: CommonOptions -> FilePath -> DaySpan -> RIO SimpleApp [RowNutrient]
+readRowNutrients
+  :: (MonadReader env m, HasLogFunc env, MonadUnliftIO m)
+  => CommonOptions
+  -> FilePath
+  -> DaySpan
+  -> m [RowNutrient]
 readRowNutrients co p ds = do
   sch <- readConfig p
-  concat <$> mapM (scheduleToTable co ds) sch
+  concat <$> mapErrorsIO (scheduleToTable co ds) sch
 
 scheduleToTable
-  :: (MonadReader env m, HasLogFunc env, MonadUnliftIO m, MonadAppError m)
+  :: (MonadReader env m, HasLogFunc env, MonadUnliftIO m)
   => CommonOptions
   -> DaySpan
   -> Schedule
   -> m [RowNutrient]
 scheduleToTable co ds Schedule {schMeal = Meal {mlIngs, mlName}, schWhen, schScale} = do
-  days <- liftExcept $ expandCronPat ds schWhen
-  let scale = fromFloatDigits $ ((fromIntegral $ length days) * schScale)
+  days <- fromEither $ expandCronPat ds schWhen
+  let scale = fromFloatDigits $ ((fromIntegral $ length days) * fromMaybe 1.0 schScale)
   rs <- concat <$> mapM (ingredientToTable co mlName) mlIngs
   return $ fmap (scaleRowNutrient scale) rs
 
@@ -321,43 +353,7 @@ toRowNutrients n i = case i of
         , rnAmount = fnAmount
         }
 
-data RowNutrient = RowNutrient
-  { rnId :: Int
-  , rnMealName :: T.Text
-  , rnDesc :: T.Text
-  , rnNutrientName :: Maybe T.Text
-  , rnNutrientId :: Maybe Int
-  , rnDerivation :: Maybe T.Text
-  , rnAmount :: Maybe Scientific
-  , rnUnit :: Maybe T.Text
-  }
-  deriving (Generic, Show)
-
-data RowSum = RowSum
-  { rsNutrientName :: T.Text
-  , rsNutrientId :: Int
-  , rsAmount :: Scientific
-  , rsUnit :: T.Text
-  }
-  deriving (Generic, Show)
-
-instance C.ToRecord RowNutrient
-
-instance C.ToRecord RowSum
-
-data Dimensional = Dimensional
-  { dimValue :: Scientific
-  , dimUnit :: Unit
-  }
-  deriving (Show, Eq)
-
-data Unit = Unit
-  { unitName :: UnitName
-  , unitBase :: Prefix
-  }
-  deriving (Show, Eq)
-
-sumRowNutrients :: [RowNutrient] -> Except T.Text [RowSum]
+sumRowNutrients :: MonadAppError m => [RowNutrient] -> m [RowSum]
 sumRowNutrients rs =
   mapM go $
     N.groupAllWith rsNutrientId $
@@ -382,7 +378,7 @@ sumRowNutrients rs =
       let (newValue, newUnit) = fromDimensional d
       return $ y {rsAmount = newValue, rsUnit = newUnit}
 
-sumDimensionals :: NonEmpty Dimensional -> Except T.Text Dimensional
+sumDimensionals :: MonadAppError m => NonEmpty Dimensional -> m Dimensional
 sumDimensionals alld@(d :| ds) = do
   d' <- foldM addDimensional d ds
   return $
@@ -407,14 +403,10 @@ count (x :| xs) =
         Nothing -> thisCount :| []
         Just ys' -> thisCount :| (N.toList (count ys'))
 
-addDimensional :: Dimensional -> Dimensional -> Except T.Text Dimensional
+addDimensional :: MonadAppError m => Dimensional -> Dimensional -> m Dimensional
 addDimensional x@(Dimensional v0 u0) y@(Dimensional v1 _)
   | uname x == uname y = return $ Dimensional (v0 + v1) u0
-  | otherwise =
-      throwE $
-        T.append
-          (T.unwords ["could not add", tshow x, "and", tshow y])
-          ": units don't match"
+  | otherwise = throwAppError $ UnitMatchError x y
   where
     uname = unitName . dimUnit
 
@@ -449,41 +441,39 @@ tunit Unit {unitName, unitBase} = T.append prefix unit
       Mega -> "M"
       Giga -> "G"
 
-parseDimensional :: Scientific -> Text -> Except T.Text Dimensional
+parseDimensional
+  :: MonadAppError m
+  => Scientific
+  -> Text
+  -> m Dimensional
 parseDimensional n u = do
   u' <- parseUnit u
   let n' = raisePower n (prefixValue $ unitBase u')
   return $ Dimensional n' u'
 
-parseUnit :: T.Text -> Except T.Text Unit
-parseUnit s = case T.splitAt 1 s of
-  ("G", rest) -> parseName Giga rest <|> def
-  ("M", rest) -> parseName Mega rest <|> def
-  ("k", rest) -> parseName Kilo rest <|> def
-  ("h", rest) -> parseName Hecto rest <|> def
-  ("d", rest) -> parseName Deci rest <|> def
-  ("c", rest) -> parseName Centi rest <|> def
-  ("m", rest) -> parseName Milli rest <|> def
-  ("µ", rest) -> parseName Micro rest <|> def
-  ("n", rest) -> parseName Nano rest <|> def
-  _ -> case T.splitAt 2 s of
-    ("da", rest) -> parseName Deca rest <|> def
-    _ -> def
+parseUnit :: MonadAppError m => T.Text -> m Unit
+parseUnit s = catchError nonUnity (const def)
   where
     def = parseName Unity s
+    nonUnity = case T.splitAt 1 s of
+      ("G", rest) -> parseName Giga rest
+      ("M", rest) -> parseName Mega rest
+      ("k", rest) -> parseName Kilo rest
+      ("h", rest) -> parseName Hecto rest
+      ("d", rest) -> parseName Deci rest
+      ("c", rest) -> parseName Centi rest
+      ("m", rest) -> parseName Milli rest
+      ("µ", rest) -> parseName Micro rest
+      ("n", rest) -> parseName Nano rest
+      _ -> case T.splitAt 2 s of
+        ("da", rest) -> parseName Deca rest
+        _ -> def
     parseName p r = case r of
       "cal" -> return $ Unit Calorie p
       "g" -> return $ Unit Gram p
       "J" -> return $ Unit Joule p
       "IU" -> return $ Unit IU p
-      _ -> throwE $ T.append "could not parse unit: " s
-
-data UnitName
-  = Gram
-  | Calorie
-  | Joule
-  | IU
-  deriving (Show, Eq)
+      _ -> throwAppError $ UnitParseError s
 
 prefixValue :: Prefix -> Int
 prefixValue Nano = -9
@@ -498,31 +488,19 @@ prefixValue Kilo = 3
 prefixValue Mega = 6
 prefixValue Giga = 9
 
-data Prefix
-  = Nano
-  | Micro
-  | Milli
-  | Centi
-  | Deci
-  | Unity
-  | Deca
-  | Hecto
-  | Kilo
-  | Mega
-  | Giga
-  deriving (Show, Eq)
-
 runSummarize :: CommonOptions -> SummarizeOptions -> RIO SimpleApp ()
-runSummarize co SummarizeOptions {soConfig, soStart, soEnd, soDays} = do
-  start <- maybe currentDay return soStart
-  let dayLen = maybe soDays (fromIntegral . diffDays start) soEnd
-  let dayspan = (start, dayLen)
+runSummarize co SummarizeOptions {soConfig, soDateInterval} = do
+  dayspan <- dateIntervalToDaySpan soDateInterval
   rs <- readRowNutrients co soConfig dayspan
-  let res = runExcept $ sumRowNutrients rs
-  case res of
-    Left e -> logError $ displayBytesUtf8 $ encodeUtf8 e
-    Right ss -> BL.putStr $ C.encodeWith tsvOptions ss
-  return ()
+  ss <- fromEither $ sumRowNutrients rs
+  BL.putStr $ C.encodeWith tsvOptions ss
+
+dateIntervalToDaySpan :: MonadUnliftIO m => DateIntervalOptions -> m DaySpan
+dateIntervalToDaySpan DateIntervalOptions {dioStart, dioEnd, dioDays} = do
+  start <- maybe currentDay return dioStart
+  let dayLen = maybe dioDays (\e -> fromIntegral $ diffDays e start) dioEnd
+  when (dayLen < 1) $ throwAppErrorIO $ DaySpanError dayLen
+  return (start, dayLen)
 
 currentDay :: MonadUnliftIO m => m Day
 currentDay = do
@@ -598,7 +576,7 @@ tsvOptions =
 
 type DaySpan = (Day, Int)
 
-expandCronPat :: DaySpan -> Cron -> AppExcept [Day]
+expandCronPat :: MonadAppError m => DaySpan -> Cron -> m [Day]
 expandCronPat b Cron {cronYear, cronMonth, cronDay, cronWeekly} =
   combineError3 yRes mRes dRes $ \ys ms ds ->
     filter validWeekday $
@@ -632,7 +610,7 @@ expandCronPat b Cron {cronYear, cronMonth, cronDay, cronWeekly} =
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
 uncurry3 f (x, y, z) = f x y z
 
-expandMDYPat :: Natural -> Natural -> MDYPat -> AppExcept [Natural]
+expandMDYPat :: MonadAppError m => Natural -> Natural -> MDYPat -> m [Natural]
 expandMDYPat lower upper (Single x) = return [x | lower <= x && x <= upper]
 expandMDYPat lower upper (Multi xs) = return $ dropWhile (<= lower) $ takeWhile (<= upper) xs
 expandMDYPat lower upper (After x) = return [max lower x .. upper]
@@ -655,24 +633,97 @@ expandMDYPat lower upper (Repeat RepeatPat {rpStart = s, rpBy = b, rpRepeats = r
 throwAppError :: MonadAppError m => AppError -> m a
 throwAppError e = throwError $ AppException [e]
 
+throwAppErrorIO :: MonadUnliftIO m => AppError -> m a
+throwAppErrorIO = fromEither . throwAppError
+
 fromDaySpan :: DaySpan -> (Day, Day)
 fromDaySpan (d, n) = (d, addDays (fromIntegral n + 1) d)
 
 dayToWeekday :: Day -> Int
 dayToWeekday (ModifiedJulianDay d) = mod (fromIntegral d + 2) 7
 
-combineError3 :: MonadAppError m => m a -> m b -> m c -> (a -> b -> c -> d) -> m d
+combineError3
+  :: (Semigroup e, MonadError e m)
+  => m a
+  -> m b
+  -> m c
+  -> (a -> b -> c -> d)
+  -> m d
 combineError3 a b c f =
   combineError (combineError a b (,)) c $ \(x, y) z -> f x y z
 
-combineError :: MonadAppError m => m a -> m b -> (a -> b -> c) -> m c
+combineError
+  :: (Semigroup e, MonadError e m)
+  => m a
+  -> m b
+  -> (a -> b -> c)
+  -> m c
 combineError a b f = combineErrorM a b (\x y -> pure $ f x y)
 
-combineErrorM :: MonadAppError m => m a -> m b -> (a -> b -> m c) -> m c
+combineErrorM
+  :: (Semigroup e, MonadError e m)
+  => m a
+  -> m b
+  -> (a -> b -> m c)
+  -> m c
 combineErrorM a b f = do
   a' <- catchError a $ \e ->
     throwError =<< catchError (e <$ b) (return . (e <>))
   f a' =<< b
 
-liftExcept :: MonadError e m => Except e a -> m a
-liftExcept = either throwError return . runExcept
+-- liftExcept :: MonadError e m => Except e a -> m a
+-- liftExcept = either throwM return . runExcept
+
+-- mapErrors :: (Traversable t, MonadAppError m) => (a -> m b) -> t a -> m (t b)
+-- -- First, record number of each action. Then try each action. On first failure,
+-- -- note it's position in the sequence, skip ahead to the untried actions,
+-- -- collect failures and add to the first failure.
+-- mapErrors f xs = mapM go $ enumTraversable xs
+--   where
+--     go (n, x) = catchError (f x) $ \e -> do
+--       es <- fmap catMaybes $ mapM (err . f) $ drop (n + 1) $ toList xs
+--       throwError $ foldr (<>) e es
+--     err x = catchError (Nothing <$ x) (pure . Just)
+
+enumTraversable :: (Num n, Traversable t) => t a -> t (n, a)
+enumTraversable = snd . L.mapAccumL go 0
+  where
+    go n x = (n + 1, (n, x))
+
+mapErrorsIO :: (Traversable t, MonadUnliftIO m) => (a -> m b) -> t a -> m (t b)
+mapErrorsIO f xs = mapM go $ enumTraversable xs
+  where
+    go (n, x) = catch (f x) $ \(AppException e) -> do
+      es <- fmap catMaybes $ mapM (err . f) $ drop (n + 1) $ toList xs
+      throwIO $ AppException $ foldr (<>) e es
+    err x = catch (Nothing <$ x) $ \(AppException es) -> pure $ Just es
+
+showError :: AppError -> [T.Text]
+showError other = case other of
+  (DaySpanError d) -> [T.unwords ["time interval must be positive, got ", tshow d, "days"]]
+  (UnitParseError u) -> [T.append "could not parse unit: " u]
+  (UnitMatchError x y) ->
+    [ T.append
+        (T.unwords ["could not add", tshow x, "and", tshow y])
+        ": units don't match"
+    ]
+  (DatePatternError s b r p) -> [T.unwords [msg, "in pattern: ", pat]]
+    where
+      pat =
+        keyVals $
+          [ (k, v)
+          | (k, Just v) <-
+              [ ("start", Just $ tshow s)
+              , ("by", Just $ tshow b)
+              , ("repeats", tshow <$> r)
+              ]
+          ]
+      msg = case p of
+        ZeroLength -> "Zero repeat length"
+        ZeroRepeats -> "Zero repeats"
+
+keyVals :: [(T.Text, T.Text)] -> T.Text
+keyVals = T.intercalate "; " . fmap (uncurry keyVal)
+
+keyVal :: T.Text -> T.Text -> T.Text
+keyVal a b = T.concat [a, "=", b]
--- src/Internal/Types/Main.hs
+++ src/Internal/Types/Main.hs
@@ -4,9 +4,67 @@ module Internal.Types.Main where
 
 import Control.Monad.Error.Class
 import Control.Monad.Trans.Except
--- import Internal.Types.Dhall
--- import Internal.Types.FDC
+import qualified Data.Csv as C
+import Data.Scientific
 import RIO
+import qualified RIO.Text as T
+
+data RowNutrient = RowNutrient
+  { rnId :: Int
+  , rnMealName :: T.Text
+  , rnDesc :: T.Text
+  , rnNutrientName :: Maybe T.Text
+  , rnNutrientId :: Maybe Int
+  , rnDerivation :: Maybe T.Text
+  , rnAmount :: Maybe Scientific
+  , rnUnit :: Maybe T.Text
+  }
+  deriving (Generic, Show)
+
+data RowSum = RowSum
+  { rsNutrientName :: T.Text
+  , rsNutrientId :: Int
+  , rsAmount :: Scientific
+  , rsUnit :: T.Text
+  }
+  deriving (Generic, Show)
+
+instance C.ToRecord RowNutrient
+
+instance C.ToRecord RowSum
+
+data Dimensional = Dimensional
+  { dimValue :: Scientific
+  , dimUnit :: Unit
+  }
+  deriving (Show, Eq)
+
+data UnitName
+  = Gram
+  | Calorie
+  | Joule
+  | IU
+  deriving (Show, Eq)
+
+data Unit = Unit
+  { unitName :: UnitName
+  , unitBase :: Prefix
+  }
+  deriving (Show, Eq)
+
+data Prefix
+  = Nano
+  | Micro
+  | Milli
+  | Centi
+  | Deci
+  | Unity
+  | Deca
+  | Hecto
+  | Kilo
+  | Mega
+  | Giga
+  deriving (Show, Eq)
 
 instance Exception AppException
 
@@ -21,6 +79,9 @@ type AppExceptT = ExceptT AppException
 
 data AppError
   = DatePatternError !Natural !Natural !(Maybe Natural) !PatternSuberr
+  | UnitMatchError !Dimensional !Dimensional
+  | UnitParseError !T.Text
+  | DaySpanError !Int
   deriving (Show)
 
 data PatternSuberr = ZeroLength | ZeroRepeats deriving (Show)
